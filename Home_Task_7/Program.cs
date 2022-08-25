a87117421202cd21cebbdf103300c2fa5ec5eaf9
MaksimMonro-create <maksim.monro@yandex.ru>
2022-08-25 23:56:41 +0300

    Home_Task_7

---
+++ Home_Task_7/Program.cs
@@ -0,0 +1,18 @@
+﻿// ДАНО ЧИСЛО ИЗ ОТРЕЗКА [10, 99]. ПОКАЗАТЬ НАИБОЛЬШУЮ ЦИФРУ ЧИСЛА.
+
+int number = new Random().Next(10, 99);
+Console.WriteLine(number);
+int number1 = number %10;
+int number2 = number - number1;
+int number3 = number2 / 10;
+//int number4 = number3 - number2;
+if (number3 > number1)
+{
+    Console.Write("Максимальное число цифры = ");
+    Console.WriteLine(number3);
+}
+else
+{
+    Console.Write("Максимальное число цифры = ");
+    Console.WriteLine(number1);
+}
