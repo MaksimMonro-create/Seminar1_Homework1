d788b1262e532c1323be9a33069a83a6c6ba563f
MaksimMonro-create <maksim.monro@yandex.ru>
2022-08-25 23:55:13 +0300

    Home_Task_4

---
+++ Home_Task_4/Program.cs
@@ -0,0 +1,14 @@
+﻿// ВЫЯСНИТЬ, ЯВЛЯЕТСЯ ЛИ ЧИСЛО ЧЕТНЫМ
+
+Console.WriteLine("Введите любое число");
+int number = int.Parse(Console.ReadLine());
+int result = number%2;
+
+if(result == 0)
+{
+    Console.WriteLine("Число четное");
+}
+    else
+    {
+        Console.WriteLine("Число нечетное");
+    }
