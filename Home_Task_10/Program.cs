3e08742e6437995a52494bd95acaf172dde60e45
MaksimMonro-create <maksim.monro@yandex.ru>
2022-08-25 23:57:55 +0300

    Home_Task_10

---
+++ Home_Task_10/Program.cs
@@ -0,0 +1,15 @@
+﻿// НАЙТИ ТРЕТЬЮ ЦИФРУ ЧИСЛА ИЛИ СООБЩИТЬ, ЧТО ЕЕ НЕТ
+
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+// string str = " ";
+string str = Convert.ToString(number);
+Console.WriteLine("Всего цифр в числе: " + str.Length);
+if(str.Length < 3)
+{
+    Console.WriteLine("Во введенном числе нет третьей цифры!");
+}
+else 
+{
+Console.WriteLine($"Третья цифра числа равна {str[2]}");
+}
