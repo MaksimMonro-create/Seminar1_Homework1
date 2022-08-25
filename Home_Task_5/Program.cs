61a0c28a8927b45fc1726c8211d85633e4cc16e4
MaksimMonro-create <maksim.monro@yandex.ru>
2022-08-25 23:55:40 +0300

    Home_Task_5

---
+++ Home_Task_5/Program.cs
@@ -0,0 +1,27 @@
+﻿// ПОКАЗАТЬ ЧЕТНЫЕ ЧИСЛА ОТ 1 ДО N
+
+Console.WriteLine("Введите первое число");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = int.Parse(Console.ReadLine());
+int result = number1;
+
+ if(number2 > number1)
+{
+    Console.WriteLine("Четные числа приведены ниже: ");
+    
+    for (result = number1;  result < number2; result++)
+    {
+        if (result%2 == 0)
+        {
+            Console.WriteLine(result);
+        }
+        
+    }
+}
+else 
+{
+    Console.WriteLine("ВТОРОЕ ЧИСЛО ДОЛЖНО БЫТЬ БОЛЬШЕ ПЕРВОГО!!!");
+}
+
+
