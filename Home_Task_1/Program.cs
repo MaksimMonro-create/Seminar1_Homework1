de08503d770ced8db16d73cd85e42d2566dbdcc0
MaksimMonro-create <maksim.monro@yandex.ru>
2022-08-25 23:53:31 +0300

    Home_Task_1

---
+++ Home_Task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// ПО ДВУМ ЗАДАННЫМ ЧИСЛАМ ПРОВЕРИТЬ, ЯВЛЯЕТСЯ ЛИ ПЕРВОЕ ЧИСЛО КВАДРАТОМ ВТОРОГО ЧИСЛА
+
+Console.WriteLine("Введите первое число");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = int.Parse(Console.ReadLine());
+int result = number1 / number2;
+if (result == number2)
+{
+    Console.WriteLine("Певое число является квадратом второго");
+}
+if (result != number2)
+{
+    Console.WriteLine("Первое число не является квадратом второго");
+}
