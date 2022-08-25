2529860601e01b6d0fafdbe7c999c08015b4e6be
MaksimMonro-create <maksim.monro@yandex.ru>
2022-08-25 23:57:07 +0300

    Home_Task_8

---
+++ Home_Task_8/Program.cs
@@ -0,0 +1,13 @@
+﻿// УДАЛИТЕ ВТОРУЮ ЦИФРУ ТРЕХЗНАЧНОГО ЧИСЛА
+int number = new Random().Next(100, 999);
+Console.Write("Трехзначное число = ");
+Console.WriteLine(number);
+int number1 = number%100;
+int number2 = number1%10;
+int number3 = number1 - number2;
+int number4 = number3 / 10;
+int number5 = number - number1;
+int number6 = number5 / 10;
+int result = number6 + number2;
+Console.Write("Новое число = ");
+Console.WriteLine(result);
